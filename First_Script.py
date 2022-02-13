7cb1a8a7cfa7bf0903d6c11816def5169722b0ae
Muhammad Farhan <farhan@Muhammads-MacBook-Pro.local>
2022-02-13 18:26:41 +0500

    First Commit

---
+++ First_Script.py
@@ -0,0 +1,18 @@
+txt = 'banana'
+
+txt.center(10)
+'Awais'.casefold()
+
+long_numebr = 10_000_000_000
+
+a,b,c = 1,2,3
+
+msg = 'abcds'
+print(msg)
+
+
+bicycles = ['A' , 'B' , 1 , 5 , f'{msg}']
+print(bicycles)
+
+bicycles.remove('B')
+
